41449f9e0ff79bde338a6f1f748163d432d59b61
AB <aasharq99@gmail.com>
2022-03-17 19:09:50 +0300

    EDDITED - the variables for the profile

--- index.js
+++ index.js
@@ -10,7 +10,11 @@
  */
 
 // Part 1 answer 👇🏻 ...
-
+let fullName = "Abdulah Ahmed Al-Sharqawi";
+let yearOfBirth =1999;
+let hobby = "Gaming";
+let funFact = "I Play the Guitar";
+const image = "https://avatars.githubusercontent.com/u/101514759?v=4";
 
 
 
@@ -29,9 +33,9 @@
  * You can uncomment a line by removing the // at the start
  */
 
-// const fullNameString = `My name is ${}`;
-// const yearOfBirthString =
-// const hobbyString =
+const fullNameString = `My name is ${fullName}`;
+const yearOfBirthString = `I am ${2022 - yearOfBirth} years old`;
+const hobbyString = `My hobby is ${hobby}`;
 
 
 
@@ -45,14 +49,18 @@ let stars = 0;
 
 function incrementBy1() {
   // Increment stars by 1 👇🏻
+  stars = stars + 1;
 }
 function decrementBy1() {
   // decrement stars by 1 👇🏻
+  stars = stars - 1;
 }
 
 function incrementBy2() {
   // Increment stars by 2 👇🏻
+  stars = stars + 2;
 }
 function decrementBy2() {
   // decrement stars by 2 👇🏻
+  stars = stars - 2;
 }
